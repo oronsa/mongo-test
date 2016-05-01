e6d5e7a8ad18eba6c3ecb9020f1a5b86421284a1
oronsa <oron570@gmail.com>
2016-05-01 15:57:08 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,11 +18,12 @@ app.use('/', routes);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public',   'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.static('../'));
+app.use('/vendor', express.static(__dirname + '../node_modules/')); //Static Route for node_modules
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,6 +55,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
